1c9fdad499c8a7801bf3d7a457eb5fe24bf4d25b
NGW <tnkngw29@gmail.com>
2021-09-18 07:13:19 +0900

    Implement pixel drawing with kernel.

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,3 +1,22 @@
-extern "C" void KernelMain() {
-    while(1) __asm__("hlt");
+#include <cstdint>
+
+/**
+ * @fn
+ * KernelMain関数
+ * 
+ * @brief
+ * カーネルのエントリポイント
+ * 
+ * @param [in] frame_buffer_base フレームバッファの先頭アドレス
+ * @param [in] frame_buffer_size フレームバッファのサイズ
+ */
+extern "C" void KernelMain(uint64_t frame_buffer_base,
+                           uint64_t frame_buffer_size) {
+  // ピクセルを描画
+  uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
+  for (uint64_t i = 0; i < frame_buffer_size; ++i) {
+    frame_buffer[i] = i % 256;
+  }
+  // 無限ループ
+  while(1) __asm__("hlt");
 }
