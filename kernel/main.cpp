16b9a217833ef1a72c79f38048831c744b40b539
NGW <tnkngw29@gmail.com>
2021-09-25 07:52:06 +0900

    Rewrite using C++ features.

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cstddef>
 
 #include "frame_buffer_config.hpp"
 
@@ -11,38 +12,81 @@ struct PixelColor {
 };
 
 /**
- * @fn
- * WritePixel関数
+ * @class
+ * PixelWriterクラス(抽象クラス)
  * 
  * @brief
- * １つの点を描画します。
- * 
- * @param [in] config FrameBufferConfig構造体
- * @param [in] x 描画したいピクセスのx座標
- * @param [in] y 描画したいピクセスのy座標
- * @param [in] c 描画したい色
- * @retval 0   成功
- * @retval 非0 失敗
+ * 1ピクセルごとの描画をおこなうクラス
  */
-int WritePixel(const FrameBufferConfig& config,
-               int x, int y, const PixelColor& c) {
-  // フレームバッファ先頭からの位置は、(余白を含む)1行あたりのピクセル数 * y + x
-  const int pixel_position = config.pixels_per_scan_line * y + x;
-  if (config.pixel_format == kPixelRGBResv8BitPerColor) {
-    uint8_t* p = &config.frame_buffer[4 * pixel_position];
+class PixelWriter {
+  public:
+    PixelWriter(const FrameBufferConfig& config) : config_{config} {
+    }
+    virtual ~PixelWriter() = default;
+    virtual void Write(int x, int y, const PixelColor& c) = 0;
+
+    protected:
+      uint8_t* PixelAt(int x, int y) {
+        return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
+      }
+
+    private:
+      const FrameBufferConfig config_;
+};
+
+/**
+ * @class
+ * RGBResv8BitPerColorPixelWriter
+ * 
+ * @brief
+ * ピクセルフォーマットがRGBResv8BitPerColorの場合のピクセル描画をおこなうクラス
+ */
+class RGBResv8ButPerColorPixelWriter : public PixelWriter {
+  public:
+    using PixelWriter::PixelWriter;
+
+  virtual void Write(int x, int y, const PixelColor& c) override {
+    auto p = PixelAt(x, y);
     p[0] = c.r;
     p[1] = c.g;
     p[2] = c.b;
-  } else if (config.pixel_format == kPixelBGRResv8BitPerColor) {
-    uint8_t* p = &config.frame_buffer[4 * pixel_position];
-    p[0] = c.b;
-    p[1] = c.g;
-    p[2] = c.r;
-  } else {
-    return -1;
   }
-  return 0;
+};
+
+/**
+ * @class
+ * BGRResv8BitPerColorPixelWriter
+ * 
+ * @brief
+ * ピクセルフォーマットがRGBResv8BitPerColorの場合のピクセル描画をおこなうクラス
+ */
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+  public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& c) override {
+      auto p = PixelAt(x, y);
+      p[0] = c.b;
+      p[1] = c.g;
+      p[2] = c.r;
+    }
+};
+
+/**
+ * 配置new演算子の定義
+ */
+void* operator new(size_t size, void* buf) {
+  return buf;
 }
+
+void operator delete(void* obj) noexcept {
+}
+
+/**
+ * グローバル変数
+ */
+char pixel_writer_buf[sizeof(RGBResv8ButPerColorPixelWriter)];
+PixelWriter* pixel_writer;
 
 /**
  * @fn
@@ -54,17 +98,28 @@ int WritePixel(const FrameBufferConfig& config,
  * @param [in] frame_buffer_config FrameBufferConfig構造体
  */
 extern "C" void KernelMain(const struct FrameBufferConfig& frame_buffer_config) {
+  // ピクセルフォーマットを判定して、対応するPixelWriterインスタンスを生成
+  switch (frame_buffer_config.pixel_format) {
+    case kPixelRGBResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf)
+        RGBResv8ButPerColorPixelWriter{frame_buffer_config};
+      break;
+    case kPixelBGRResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf)
+        BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+      break;
+  }
   // 背景ピクセルを描画
   for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
     for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-      WritePixel(frame_buffer_config, x, y, {255, 255, 255});
+      pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
   // 仮想ウィンドウのピクセル描画
   // 位置は、左上から100,100。大きさは、200 x 100。色は(0, 255, 0)=緑
   for (int x = 0; x < 200; ++x) {
     for(int y = 0; y < 100; ++y) {
-      WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+      pixel_writer->Write(100 + x, 100 + y, {0, 255, 0});
     }
   }
   // 無限ループ
