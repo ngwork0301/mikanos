b4f568e9787d22bbe4d89eea4630e3cacc4893fc
NGW <tnkngw29@gmail.com>
2021-12-13 06:51:12 +0900

    Implement finding FADT.

--- kernel/acpi.cpp
+++ kernel/acpi.cpp
@@ -66,6 +66,52 @@ namespace acpi {
     return true;
   }
 
+  /**
+   * @fn
+   * DescriptionHeader::IsValid関数
+   * 
+   * @brief 
+   * XSDTのヘッダが有効か検査する
+   * @param expected_signature 期待する文字列
+   * @return true 有効
+   * @return false 不正
+   */
+  bool DescriptionHeader::IsValid(const char* expected_signature) const {
+    // 期待する文字列と一致することを確認
+    if (strncmp(this->signature, expected_signature, 4) != 0) {
+      Log(kDebug, "invalid signature: %.4s\n", this->signature);
+      return false;
+    }
+    // チェックサムをとって0でなければfalseを返す
+    if (auto sum = SumBytes(this, this->length); sum != 0) {
+      Log(kDebug, "sum of %u bytes must be 0: %d\n", this->length, sum);
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * @brief 
+   * XSDTのエントリにアクセスする添字演算子
+   */
+  const DescriptionHeader& XSDT::operator[](size_t i) const {
+    auto entries = reinterpret_cast<const uint64_t*>(&this->header + 1);
+    return *reinterpret_cast<const DescriptionHeader*>(entries[i]);
+  }
+
+  /**
+   * @fn
+   * XSDT::Count関数
+   * 
+   * @brief 
+   * XSDTが保持するデータ構造のアドレスの個数を返す。
+   * @return size_t XSDTが保持するデータ構造のアドレスの個数
+   */
+  size_t XSDT::Count() const {
+    return (this->header.length - sizeof(DescriptionHeader)) / sizeof(uint64_t);
+  }
+
+  const FADT* fadt;
 
   /**
    * @fn
@@ -77,8 +123,32 @@ namespace acpi {
    * @param [in] rsdp RSDP構造体へのポインタ
    */
   void Initialize(const RSDP& rsdp) {
+    // RSDPの検証
     if (!rsdp.IsValid()) {
       Log(kError, "RSDP is invalid.\n");
+      exit(1);
+    }
+
+    // XSDTの取得と検証
+    const XSDT& xsdt = *reinterpret_cast<const XSDT*>(rsdp.xsdt_address);
+    if (!xsdt.header.IsValid("XSDT")) {
+      Log(kError, "XSDT is invalid.\n");
+      exit(1);
+    }
+
+    fadt = nullptr;
+    // XSDTのエントリを一つずつ調べてFADTをみつける。
+    for (int i = 0; i < xsdt.Count(); ++i) {
+      const auto& entry = xsdt[i];
+      if (entry.IsValid("FACP")) {  // FACP is the signature of FADT
+        fadt = reinterpret_cast<const FADT*>(&entry);
+        break;
+      }
+    }
+
+    // FADTが見つからなかったら、エラー
+    if (fadt == nullptr) {
+      Log(kError, "FADT is not found.\n");
       exit(1);
     }
   }
